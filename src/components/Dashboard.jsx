a767ea0e22f944923e5667788ca1d0118bcd3da9
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-04-05 19:29:28 +0200

    basic layout done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,31 +2,78 @@ import React from "react";
 import { useState } from "react";
 import { useUserContext } from "../store/UserProvider";
 
-function Dashboard({noUserDetected}) {
-
+function Dashboard({ noUserDetected }) {
   const { user } = useUserContext();
-  const [currentDashboardScreen, setCurrentDashboardScreen] = useState('default')
+  const [currentDashboardScreen, setCurrentDashboardScreen] =
+    useState("default");
 
   if (!user) {
-    noUserDetected()
+    noUserDetected();
   }
 
   return (
     <main id="dashboard">
       <aside>
-        <h1>Dashboard</h1>
-        <button onClick={() => {setCurrentDashboardScreen('updateUserData')}}>Perfil d'usuari</button>
+        <h2>Dashboard</h2>
+        <button
+          onClick={() => {
+            setCurrentDashboardScreen("updateUserData");
+          }}
+        >
+          Perfil d'usuari
+        </button>
         {(user.role === "Administrador" || user.role === "Bibliotecari") && (
           <>
-            <button onClick={() => {setCurrentDashboardScreen('import')}}>Importació d'usuaris</button>
+            <button
+              onClick={() => {
+                setCurrentDashboardScreen("import");
+              }}
+            >
+              Importació d'usuaris
+            </button>
             <a href="/admin">Administració</a>
           </>
         )}
       </aside>
       <section>
-          {currentDashboardScreen === 'default' && <p>Esto es la página de inicio</p>}
-          {currentDashboardScreen === 'updateUserData' && <p>Esto es la página de modificación de usuario</p>}
-          {currentDashboardScreen === 'import' && <p>Esto es la página de importación</p>}
+        {currentDashboardScreen === "default" && (
+          <>
+            <h2>Inici</h2>
+            <p>Esto es la página de inicio</p>
+          </>
+        )}
+        {currentDashboardScreen === "updateUserData" && (
+          <>
+            <h2>Perfil</h2>
+            <p>Esto es la página de modificación de usuario</p>
+          </>
+        )}
+        {currentDashboardScreen === "import" && (
+          <>
+            <h2>Importación</h2>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+            <p>Esto es la página de importación</p>
+          </>
+        )}
       </section>
     </main>
   );
