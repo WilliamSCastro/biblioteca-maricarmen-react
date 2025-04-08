ca0075cffb5b06dcab0cd9377530e3bbf180de30
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-04-08 15:38:50 +0200

    Profile update DONE

--- src/components/dashboard/Profile/Profile.jsx
+++ src/components/dashboard/Profile/Profile.jsx
@@ -4,12 +4,12 @@ import { ProfileForm } from "./ProfileForm";
 
 export default function Profile() {
   const { user, handleUserUpdates } = useUserContext();
-  
-
+  const url = `http://127.0.0.1:8000${user.imatge_url}`;
+  console.log(url)
   return (
     <>
       <div id="title-box">
-        {user.imatge_url && <img src={user.imatge_url} alt="user_image" />   }
+        {user.imatge_url && <img src={url} alt="user_image" />}
         <h2>{user.username}</h2>
       </div>
       <UserInfo user={user} />
