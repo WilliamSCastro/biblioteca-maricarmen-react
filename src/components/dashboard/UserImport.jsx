51348261806f066a0ad041c299bcc0687fab2ead
JorgeCortes02 <120786819+JorgeCortes02@users.noreply.github.com>
2025-04-10 12:05:13 +0200

    cambios

--- src/components/dashboard/UserImport.jsx
+++ src/components/dashboard/UserImport.jsx
@@ -25,7 +25,7 @@ const UserImport = () => {
       setError(data?.error || error || "Error desconegut. Contacta amb l'administrador");
       return;
     }
-<<<<<<< HEAD
+
     
     setSummary(data);
     }
@@ -66,48 +66,10 @@ const UserImport = () => {
   ;
   
   };
-=======
->>>>>>> Jorge
 
-    setSummary(data);
-  }
-  return (
-    <div className="importContainer">
-      <h2>Importació massiva d'usuaris</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="csvFile">Selecciona el fitxer CSV:</label>
-        <input
-          type="file"
-          id="csvFile"
-          accept=".csv"
-          onChange={handleFileChange}
-        />
-        <button type="submit">Importar usuaris</button>
-      </form>
+  
 
-      {error && (
-        <div className="errorMessage">
-          <p>{error}</p>
-        </div>
-      )}
 
-      {summary && (
-        <div className="summaryMessage">
-          <p>{summary.message}</p>
-          {summary.errors && summary.errors.length > 0 && (
-            <ul>
-              {summary.errors.map((errMsg, idx) => (
-                <li key={idx}>{errMsg}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-      )}
-    </div>
-  );
-  ;
-
-};
 
 
 export default UserImport;
