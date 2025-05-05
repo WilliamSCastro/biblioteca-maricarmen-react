44fb3aa549fe96d0b94d15e7fe090395cbf49728
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-05-05 19:44:30 +0200

    first fix of merge

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 import {ERROR_TYPES, RESPONSE_TYPES} from "../constants";
 
 // API URL
-<<<<<<< HEAD
-const API = `http://127.0.0.1:8000/api`;
+// const API = `http://127.0.0.1:8000/api`;
 //const API = `https://biblioteca4.ieti.site/api`;
-=======
 
 const API = window.location.origin + "/api"
->>>>>>> dev
+console.log("API URL:", API);
 
 export const getBooks = () => {
   console.log(`llamando API getBooks en ${API}/llibres/`);
